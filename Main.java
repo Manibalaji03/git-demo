de9a245bd75b78a7ff70c862ba1b3c1b122e82d5
vigneshvaran@brimmatech.com <vigneshvaran@brimmatech.com>
2024-10-29 16:35:33 +0530

    vicky added

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void Main(String[] args){
         System.out.println("this is my first commiit - manibalaji");
-        System.out.print("vicky - commit");
+        System.out.print("vicky");
     }
 }
