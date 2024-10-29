cf91897fd49e8f03a7cdcf625a6a1fadde02b7b2
Venkadesh23 <venkadesh.k@brimmatech.com>
2024-10-29 21:31:45 +0530

    try

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void Main(String[] args){
         System.out.println("this is my first commiit - manibalaji");
         System.out.print("re-committed - dharmar");
+        System.out.print("re-committed - venkadesh_K");
+        System.out.print(1+2);
     }
 }
