35946c988aa03e881a1cc2c2ba3654b059a44f15
karthick-10 <karthick.r@brimmatech.com>
2024-10-29 17:11:45 +0530

    added the line karthick123

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void Main(String[] args){
         System.out.println("this is my first commiit - manibalaji");
         System.out.print("vicky-123");
+        System.out.print("Karthick-123");
     }
 }
