78deccef4948bcd3ecb052bc958a162690867d9f
vigneshvaran@brimmatech.com <vigneshvaran@brimmatech.com>
2024-10-30 10:38:22 +0530

    new line add

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void Main(String[] args){
         System.out.println("this is my first commiit - manibalaji");
         System.out.print("vicky-123");
+        System.out.println("123");
+
     }
 }
